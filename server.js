97f62c9002e5545be43e463fc67e3314bdae1abf
Mauricio Oliveira <mauricio.oliveira@nabarrete.local>
2025-08-21 09:22:10 -0400

    refactor: remove importação não utilizada do cors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 console.log('--- SERVER STARTING - DEPLOY VERSION 7 ---');
 const express = require('express');
-const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
